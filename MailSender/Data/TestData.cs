eae2c77f0719ead3c82f009c8120865d28845000
Lloy94 <mcdos14@narod.ru>
2021-06-16 16:00:21 +0500

    Добавлены обработчки событий для кнопок первого окна.

--- MailSender/Data/TestData.cs
+++ MailSender/Data/TestData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace MailSender.Data
                UseSSL = i % 2 == 0
            })
            .ToList();
+
 
         public static List<Sender> Senders { get; } = Enumerable.Range(1, 10)
            .Select(i => new Sender
--- MailSender/WpfMailSender.xaml.cs
+++ MailSender/WpfMailSender.xaml.cs
@@ -38,5 +38,48 @@ namespace MailSender
 
         private void About_OnClick(object Sender, RoutedEventArgs E) =>
                 MessageBox.Show("Рассыльщик почты", "О программе", MessageBoxButton.OK);
+
+        private void ServerAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Data.TestData.Servers.Add(new Models.Server());
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = Data.TestData.Servers;
+        }
+
+        private void ServerDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Data.TestData.Servers.RemoveAt(ServersList.SelectedIndex);
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = Data.TestData.Servers;
+        }
+
+        private void SenderAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Data.TestData.Senders.Add(new Models.Sender());
+        }
+
+        private void SenderDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Data.TestData.Senders.RemoveAt(SendersList.SelectedIndex);
+            SendersList.ItemsSource = null;
+            SendersList.ItemsSource = Data.TestData.Senders;
+        }
+
+        private void RecipientAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Data.TestData.Recipients.Add(new Models.Recipient());
+        }
+
+        private void RecepientRemove_Click(object sender, RoutedEventArgs e)
+        {
+            Data.TestData.Recipients.RemoveAt(RecepientList.SelectedIndex);
+            RecepientList.ItemsSource = null;
+            RecepientList.ItemsSource = Data.TestData.Recipients;
+        }
+
+        private void Shedule_Click(object sender, RoutedEventArgs e)
+        {
+            tbMain.SelectedIndex = 1;
+        }
     }
 }
