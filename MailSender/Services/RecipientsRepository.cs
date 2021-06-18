b62102749d425d350d5e8fbd8bbab71f2ea0d9b2
Lloy94 <mcdos14@narod.ru>
2021-06-18 17:40:55 +0500

    Добавлен репозиторий отправителей

---
+++ MailSender/Services/RecipientsRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender.Models;
+
+namespace MailSender.Services
+{
+    public class RecipientsRepository
+    {
+        private List<Recipient> _Recipients;
+
+        public RecipientsRepository()
+        {
+            _Recipients = Enumerable.Range(1, 10)
+               .Select(i => new Recipient
+               {
+                   Id = i,
+                   Name = $"Получатель - {i}",
+                   Address = $"sender-{i}@server.ru",
+                   Description = $"Описание получателя {i}",
+               })
+               .ToList();
+        }
+
+        public IEnumerable<Recipient> GetAll() => _Recipients;
+
+        public Recipient Create(int Id, string Name, string Address, string Description)
+        {
+            var recipient = new Recipient
+            {
+                Id = Id,
+                Name = Name,
+                Address = Address,
+                Description = Description
+            };
+            Add(recipient);
+            return recipient;
+        }
+
+        public void Add(Recipient recipient)
+        {
+            _Recipients.Add(recipient);
+        }
+
+        public void Remove(Recipient recipient)
+        {
+            _Recipients.Remove(recipient);
+        }
+    }
+}
+
