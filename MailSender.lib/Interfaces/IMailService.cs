6d64cc5dacb3cd961364e1e331d24f56ecc60428
Lloy94 <mcdos14@narod.ru>
2021-06-18 21:12:36 +0500

    Исправлен интерфейс отправки почты/ перехват ошибок.

--- MailSender.lib/Interfaces/IMailService.cs
+++ MailSender.lib/Interfaces/IMailService.cs
@@ -14,6 +14,7 @@ namespace MailSender.Interfaces
     {
         void SendEmail(Sender _sender, Recipient _recipient, Message _message, Server _server) 
         {
+            if (_message == null|| _recipient == null) return;
             using var message = new MailMessage(_sender.Address, _recipient.Address);
             message.Subject = _message.Title;
             message.Body = _message.Text;
@@ -26,6 +27,13 @@ namespace MailSender.Interfaces
                     Password = _server.Password,
                 }
             };
+            try
+            {
+                client.Send(message);
+
+            }
+            catch (SmtpException smtp_exception) { }
+
         }
     }
 }
