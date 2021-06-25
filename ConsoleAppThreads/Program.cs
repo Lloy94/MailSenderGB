cf214c41e251f992ef7a122389e6bfa9bb4dc1d5
Lloy94 <Mcdos14@narod.ru>
2021-06-25 20:45:40 +0500

    Добавлен проект с потоками

---
+++ ConsoleAppThreads/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Numerics;
+using System.Threading;
+
+namespace ConsoleAppThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = int.Parse(Console.ReadLine());
+            Thread myThread1 = new Thread(() => Summ(n));
+            myThread1.Start();
+            Thread myThread2 = new Thread(() => Factorial(n));
+            myThread2.Start();
+            Console.ReadLine();
+        }
+
+        public static BigInteger Factorial(int n)
+        {
+            var factorial = new BigInteger(1);
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+                Console.WriteLine($"Вычисление факториала в первом потоке - {factorial}");
+                Thread.Sleep(50);
+            }
+
+            return factorial;
+        }
+
+        private static void Summ(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+                Console.WriteLine($"Вычисление суммы чисел во втором потоке - {sum}");
+                Thread.Sleep(50);
+            }
+        }
+
+
+    }
+}
