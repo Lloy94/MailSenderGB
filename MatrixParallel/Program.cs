43b36249494115957d990dc47d41226b5f2205f7
Lloy94 <Mcdos14@narod.ru>
2021-07-01 05:38:49 +0500

    Добавлены тестовые методы

--- MatrixParallel/Program.cs
+++ MatrixParallel/Program.cs
@@ -8,7 +8,7 @@ namespace MatrixParallel
 {
     class Program
     {
-        private static int [,] MatrixCreate()
+        private static int[,] MatrixCreate()
         {
             Random rand = new();
             var matrix = new int[100, 100];
@@ -33,18 +33,18 @@ namespace MatrixParallel
         {
             matrix[i, j] = 0;
             for (int k = 0; k < 100; k++)
-            {               
+            {
                 matrix[i, j] += matrix1[i, k] * matrix2[k, j];
             }
             thread_id = Thread.CurrentThread.ManagedThreadId;
             return matrix[i, j];
         }
 
-        private static void Progress(int i,int thread_id)
+        private static void Progress(int i, int thread_id)
         {
             Console.WriteLine($"получено число {i} в потоке {thread_id}");
         }
-        private static async Task ParralellMatrixMulti(int[,] matrix1, int [,] matrix2)
+        private static async Task ParralellMatrixMulti(int[,] matrix1, int[,] matrix2)
         {
             int[,] matrix3 = new int[100, 100];
             var tasks = new List<Task>();
@@ -52,13 +52,53 @@ namespace MatrixParallel
             {
                 for (int j = 0; j < 99; j++)
                 {
-                    tasks.Add(Task.Run(() => {
-                    matrix3[i, j] = MatrixMulti(matrix3, matrix1, matrix2, i, j, out int thread_id);
+                    tasks.Add(Task.Run(() =>
+                    {
+                        matrix3[i, j] = MatrixMulti(matrix3, matrix1, matrix2, i, j, out int thread_id);
                         Progress(matrix3[i, j], thread_id);
-                    }));                                    
+                    }));
                 }
             }
             await Task.WhenAll(tasks).ConfigureAwait(false);
         }
+       
+       /* static void Main(string[] args)
+        {
+            var matrix1 = MatrixCreate();
+            var matrix2 = MatrixCreate();
+            ParralellMatrixMulti(matrix1, matrix2);
+            Console.WriteLine("работа программы завершена");
+        }
+
+        private static int MatrixMulti(int[,] matrix, int[,] matrix1, int[,] matrix2, int i, int j, out int thread_id)
+        {
+            matrix[i, j] = 0;
+            for (int k = 0; k < 100; k++)
+            {
+                matrix[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+            thread_id = Thread.CurrentThread.ManagedThreadId;
+            return matrix[i, j];
+        }
+
+        private static void Progress(int i, int thread_id)
+        {
+            Console.WriteLine($"получено число {i} в потоке {thread_id}");
+        }
+        private static int[,] ParralellMatrixMulti(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] matrix3 = new int[100, 100];
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    matrix3[i, j] = MatrixMulti(matrix3, matrix1, matrix2, i, j, out int thread_id);
+                    Progress(matrix3[i, j], thread_id);
+
+                }
+            }
+            return matrix3;
+        }
+        */
     }
 }
